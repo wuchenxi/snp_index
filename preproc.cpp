8811a79a5c1ea6646be5a4d29b702afdaeefa391
Chenxi Wu <cw538@cornell.edu>
2018-01-06 01:56:23 -0500

    Add files via upload

--- preproc.cpp
+++ preproc.cpp
@@ -2,15 +2,17 @@
 #include <cstdlib>
 #include <cstring>
 
-const int chr=3;
+const int chr=7;
 
 char buf[100000];
+char buf2[100000];
 double avg[6210];
 
 int main(int argc, char* argv[]){
   FILE* idx=fopen("merbin_infw.txt","rb");
-  FILE* in=fopen("bin3.geno","rb");
-  FILE* out=fopen("sum3.geno","wb");
+  FILE* in=fopen("bin7.geno","rb");
+  FILE* out=fopen("sum7.geno","wb");
+  fgets(buf,100000,in);
   for(int i=0;i<30350;i++){
     int j; int c; double l; double r;
     fscanf(idx, "%d", &j);
@@ -18,23 +20,28 @@ int main(int argc, char* argv[]){
     fscanf(idx, "%lf", &l);
     fscanf(idx, "%lf", &r);
     if(c==chr){
-    printf("%d: (%g, %g)\n", c, l, r);
+    printf("%d: (%lf, %lf)\n", c, l, r);
     for(int i=0;i<6210;i++)avg[i]=0;
     int len=0;
+    int id;
     while(true){
-        if(!fgets(buf,100000,in))break;
-	int id; 
-	char* p=strtok(buf,"\t");
+      strcpy(buf2,buf);
+	char* p=strtok(buf2,"\t");
 	sscanf(p,"%d",&id);
+	//printf("%d\n",id);
 	if(id>=l&&id<=r){
 	  len++;
 	  for(int j=0;j<6210;j++){
 	    p=strtok(NULL,"\t");avg[j]+=(int)(p[0]-'0');
 	  }
 	}
+	if(id>=r)break;
+      if(!fgets(buf,100000,in))break; 
     }
-    for(int j=0;j<6209;j++)fprintf(out,"%g\t",avg[j]/len);
-    fprintf(out,"%g\n",avg[6209]/len);
+    if(len)
+      {printf("%d, %d\n",len,id);
+	for(int j=0;j<6209;j++)fprintf(out,"%g\t",avg[j]/len);
+	fprintf(out,"%g\n",avg[6209]/len);}
     }
   }
   return 0;
