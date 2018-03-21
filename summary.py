dae758e2ab58544b6938b17a70f87868f1ebb9c0
Chenxi Wu <cw538@cornell.edu>
2018-03-21 03:37:42 -0400

    Add files via upload

--- summary.py
+++ summary.py
@@ -1,9 +1,10 @@
-#Usage: python3 summary.py > output_file_name
-#For example: python3 summary.py > out
-#Input files: emaize_5M.add.gz merbin_infw.txt snpfunc_5M_emaize.gz
+#Usage: python summary.py > output_file_name
+#For example: python summary.py > out
+#Input files: emaize_5M.add.gz merbin_infw.txt snpfunc_5M_emaize.gz 200kpos.txt
 #Then "out" is a space separated snp-index file and we can use various ML methods to analyze it.
 
 import gzip
+import sys
 snp_file_name="emaize_5M.add.gz"
 intervals_file_name="merbin_infw.txt"
 comments_file_name="snpfunc_5M_emaize.gz"
@@ -11,14 +12,14 @@ exclude_file_name="200kpos.txt"
 weights=[1,1,1,1,2,3] 
 
 def split_name(s):
-    l=str.split(s,".")
+    l=s.split(".")
     return (int((l[0])[3:]), int((l[1])[2:]))
 
 def int_end(s):
-    l=str.split(s.decode("utf-8"),"\t")
+    l=s.decode("ascii").split("\t")
     return (int(l[1]),float(l[3]))
 def ind_exclude(s):
-    l=str.split(s.decode("utf-8"),"\t")
+    l=s.decode("ascii").split("\t")
     return (int(l[0]),float(l[1]))
 
 snp_f=gzip.open(snp_file_name)
@@ -28,24 +29,32 @@ next(interval_f)
 curint=int_end(next(interval_f))
 func_f=gzip.open(comments_file_name)
 next(func_f)
-func_l=str.split((next(func_f)).decode("utf-8"),",")
+func_l=(next(func_f)).decode("ascii").split(",")
 cursum=[0]*6210
 
 exclude_f=open(exclude_file_name,"rb")
 curex=ind_exclude(next(exclude_f))
 
 for snp in snp_f:
-    snp_l=str.split(snp.decode("utf-8"),",")
+    snp_l=snp.decode("ascii").split(",")
     snp_name=split_name(snp_l[0])
     if snp_name==curex:
         continue
     while snp_name>curex:
-        curex=ind_exclude(next(exclude_f))
+        exclude_s=exclude_f.readline()
+        if exclude_s==b'':
+            curex=(11,0)
+        else:
+            curex=ind_exclude(exclude_s)
     snp_val=[int(x) for x in snp_l[3:]]
     #print(snp_name)
     snp_weight=0
     while split_name(func_l[0])<snp_name:
-        func_l=str.split((next(func_f)).decode("utf-8"),",")
+        func_s=func_f.readline()
+        if func_s==b'':
+            sys.exit(0)
+        else:
+            func_l=(func_s).decode("ascii").split(",")
     if split_name(func_l[0])==snp_name:
         snp_weight=weights[int(func_l[2])-1]
     else:
@@ -55,5 +64,9 @@ for snp in snp_f:
     else:
         print(' '.join(map(str,cursum)))
         while curint<=snp_name:
-            curint=int_end(next(interval_f))
+            interval_s=next(interval_f)
+            if interval_s==b'':
+                sys.exit(0)
+            else:
+                curint=int_end(interval_s)
         cursum=[snp_weight*y for y in snp_val]
