e1d08702e348043212e36158a8473c06f9cedc7e
Chenxi Wu <cw538@cornell.edu>
2018-08-20 13:33:28 -0400

    Update brr_val.py

--- brr_val.py
+++ brr_val.py
@@ -1,9 +1,9 @@
 #usage: Put the following input files at the same folder as this file:
 # geno: space separated snp-index file, first column is number of snp in each interval.
 # pheno: 3 columns of phenotype, tab separated
-# parents.txt: 2 columns of parental index
+# parents.txt: 2 columns of parental index, tab separated
 # sample_idx: Index file indicating the train/test/validation split
-# Then do python brr_val.py geno pheno parents.txt sample_idx 2
+# Then do python3 brr_val.py geno pheno parents.txt sample_idx 2
 # The last number in the printout will be the covariance of predicted value on validation set
 
 import csv
@@ -16,26 +16,26 @@ import sys
 ## Main program
 
 n_s = 6210
-y = SP.array(list(csv.reader(open(sys.argv[2],'rb'),
+y = SP.array(list(csv.reader(open(sys.argv[2],'r'),
                              delimiter='\t'))).astype(float)
 y = y[:,int(sys.argv[5])].reshape((n_s,1))#feature 3
 
 
 # load genotype
-X = SP.array(list(csv.reader(open(sys.argv[1],'rb'),delimiter=' '))).astype(float)
+X = SP.array(list(csv.reader(open(sys.argv[1],'r'),delimiter=' '))).astype(float)
 
 n_f = X.shape[0]
 X1=X[:,1:]
-for i in xrange(n_f):
+for i in range(n_f):
     X1[i]=(X1[i]-(X1[1]).mean())/X[i,0]
 X = X1.T
-print X
-print X.shape
+print(X)
+print(X.shape)
 
-parents = SP.array(list(csv.reader(open(sys.argv[3],'rb'),
+parents = SP.array(list(csv.reader(open(sys.argv[3],'r'),
                                    delimiter='\t'))).astype(int)
 
-split_idx=list(csv.reader(open(sys.argv[4],'rb'),delimiter='\t'))
+split_idx=list(csv.reader(open(sys.argv[4],'r'),delimiter='\t'))
 train=[x for x in range(6210) if split_idx[x]==["2"]]
 valid=[x for x in range(6210) if split_idx[x]==["1"]]
 
@@ -45,7 +45,7 @@ for i in train:
         idxf=idxf+[parents[i,1]]
 test1=[]
 test2=[]
-for i in xrange(n_s):
+for i in range(n_s):
     if parents[i,1] not in idxf:
         test2=test2+[i]
     elif i not in train:
@@ -61,11 +61,11 @@ def train_and_eval_with_select(Xtrain,Xtest,ytrain):
     from sklearn.feature_selection import f_regression
     F, p=f_regression(Xtrain,ytrain)
     features=[]
-    for i in xrange(Xtrain.shape[1]):
+    for i in range(Xtrain.shape[1]):
         if p[i]<0.001:
             print p[i]
             features+=[i]
-    print len(features)
+    print(len(features))
     Xtrain=Xtrain[:,features]
     Xtest=Xtest[:,features]
     reg=LM.BayesianRidge(n_iter=30)
@@ -85,4 +85,4 @@ yhat[test1]=train_and_eval(X[train], X[test1], yhat[train])
 yhat[test2]=train_and_eval(X[train2], X[test2], yhat[train2])
 
 summ=[(y[test[i]]-yhat[test[i]])**2 for i in range(len(test)) if test[i] in valid]
-print sum(summ)
+print(sum(summ))
