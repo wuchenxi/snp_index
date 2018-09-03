5857f8a2422cbfc80b2b7e06d0f8d2b56233a6f1
Chenxi Wu <cw538@cornell.edu>
2018-09-02 22:22:48 -0400

    Update weight.py

--- weight.py
+++ weight.py
@@ -1,4 +1,4 @@
-#Usage: cat Sum_Chrs | python corr.py > Sum
+#Usage: cat Sum_Chrs | python weight.py > Sum
 #Here 2 means the third phenotype
 #pheno.csv is tab separated, with unknowns as "nan" or "NA"
 
